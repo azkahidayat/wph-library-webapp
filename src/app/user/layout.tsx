71e7bdc5e8fdd13b2eac1240aab6b77a03a9d98c
azkahidayat <azka.reksalegora12@gmail.com>
2025-11-29 09:44:36 +0700

    add feat;header,footer

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { UserFooter, UserNavbar } from './components';
 
 const UserLayout = () => {
   return (
-    <div className='flex min-h-screen items-center justify-center'>
-      <Outlet />
+    <div className='flex min-h-screen flex-col'>
+      <UserNavbar />
+      <main className='flex-1 pt-[68px] md:pt-[84px] flex flex-col'>
+        <Outlet />
+      </main>
+      <UserFooter />
     </div>
   );
 };
