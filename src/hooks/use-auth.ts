727521a4e46c74df25d6dc5e586511c72c6d315e
azkahidayat <azka.reksalegora12@gmail.com>
2025-11-29 22:23:26 +0700

    add feat;cart

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,9 +1,11 @@
+import { getErrorMessage } from '@/api';
 import type { LoginRequest, RegisterRequest } from '@/schema';
 import { authService } from '@/service/auth.service';
 import { useAppDispatch } from '@/store';
-import { setCredentials } from '@/store/slices';
+import { clearAuth, setCredentials } from '@/store/slices';
 import type { LoginSuccessResponse } from '@/type';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
 
 export const authKeys = {
@@ -40,3 +42,22 @@ export const useRegister = () =>
     mutationFn: (req: RegisterRequest) => authService.register(req),
     onSuccess: () => toast.info('Registration successful! Please log in.'),
   });
+
+export const useLogout = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  return async () => {
+    try {
+      dispatch(clearAuth());
+      localStorage.clear();
+      await queryClient.cancelQueries();
+      queryClient.clear();
+
+      navigate('/auth/login', { replace: true });
+    } catch (error) {
+      toast.error(getErrorMessage(error));
+    }
+  };
+};
